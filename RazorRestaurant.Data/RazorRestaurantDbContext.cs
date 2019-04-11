d2a6f6b26c3a310725f4dcafbf4e9698ca14d886
Irvin Reyes <reyes.yerp@gmail.com>
2019-04-10 21:38:10 -0500

    Add Entity Framework DbContext

---
+++ RazorRestaurant.Data/RazorRestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorRestaurant.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorRestaurant.Data
+{
+    public class RazorRestaurantDbContext : DbContext
+    {
+        public RazorRestaurantDbContext(DbContextOptions<RazorRestaurantDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
